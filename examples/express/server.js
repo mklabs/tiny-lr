8f1a13b3370c6172a3bb74e927748821bfda697d
Mickael Daniel <daniel.mickael@gmail.com>
2014-05-17 20:44:01 +0200

    Init wd tests

--- examples/express/server.js
+++ examples/express/server.js
@@ -9,49 +9,9 @@ var debug   = require('debug')('tinylr:server');
 
 process.env.DEBUG = process.env.DEBUG || 'tinylr*';
 
-var app = express();
+var app = require('./app');
 
-function logger(fmt) {
-  fmt = fmt || '%s - %s';
-
-  return function logger(req, res, next) {
-    debug(fmt, req.method, req.url);
-    next();
-  }
-}
-
-function throttle(delay, fn) {
-  var now = Date.now();
-
-  return function() {
-    var from = Date.now();
-    var interval = from - now;
-    if (interval < delay) return;
-    now = from;
-    fn.apply(this, arguments);
-  };
-}
-
-var watch = (function watch(em) {
-  em = em || new (require('events').EventEmitter)();
-
-  em.on('rename', function(file) {
-    tinylr.changed(file);
-  });
-
-  fs.watch(path.join(__dirname, 'styles/site.css'), throttle(200, function(ev, filename) {
-    em.emit(ev, filename);
-  }));
-
-  return watch;
-})();
-
-app
-  .use(logger())
-  .use(body())
-  .use('/', express.static(path.join(__dirname)))
-  .use(tinylr.middleware({ app: app }))
-  .listen(port, function(err) {
-    if (err) throw err;
-    debug('listening on %d', port);
-  });
+app.listen(port, function(err) {
+  if (err) throw err;
+  debug('listening on %d', port);
+});
