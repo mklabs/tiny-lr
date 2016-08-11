be5e5006b49d15ac4f66eff7e138d35fb7ca27d5
Mickael Daniel <daniel.mickael@gmail.com>
2016-08-11 20:52:17 +0200

    dashboard - slightly larger card on desktop / tablet

--- cli/assets/view.js
+++ cli/assets/view.js
@@ -2,7 +2,7 @@ class View extends Yov {
 
   template ({id, url = ''}) {
     return this.hx`
-      <div class="dashboard-card mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--8-col-desktop">
+      <div class="dashboard-card mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-cell--10-col-desktop">
         <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
           <h2 class="dashboard-card-title mdl-card__title-text">
             <a href="${url}">${url}</a>
