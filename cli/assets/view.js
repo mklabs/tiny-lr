c023689a3cab9a0dad2906f42c9f6610af14b1bb
Mickael Daniel <daniel.mickael@gmail.com>
2016-08-11 20:14:16 +0200

    dashboard - add <a/> tag around card url

--- cli/assets/view.js
+++ cli/assets/view.js
@@ -4,7 +4,9 @@ class View extends Yov {
     return this.hx`
       <div class="dashboard-card mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--8-col-desktop">
         <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
-          <h2 class="dashboard-card-title mdl-card__title-text">${url}</h2>
+          <h2 class="dashboard-card-title mdl-card__title-text">
+            <a href="${url}">${url}</a>
+          </h2>
         </div>
         <div class="mdl-card__supporting-text mdl-color-text--grey-600">
           id: ${id}
