6a53d99712b3a4bdc7730bbdacd4aba38439d089
Mickael Daniel <daniel.mickael@gmail.com>
2016-08-11 20:08:22 +0200

    dashboard - send create events on info message to also have url data

--- cli/assets/index.js
+++ cli/assets/index.js
@@ -1,60 +1,44 @@
 const View = require('./view');
 const debug = require('debug')('tinylr:dashboard');
 
-// Apply a small delay to let livereload.js send a message with client info
-// todo: remove settimeout by listening to websocket message or any other
-// means, maybe pooling
+localStorage.debug = 'tinylr:*'
+let views = {};
 
-let process = LiveReload.connector.protocolParser.process;
+fetch('/clients')
+  .then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    // todo: only display current page if on dashboard
+    // json.clients.forEach(createClient);
 
-LiveReload.connector.protocolParser.process = function() {
-  console.log('process', arguments);
-  return process.apply(this, arguments);
-};
-
-setTimeout(() => {
-  localStorage.debug = 'tinylr:*'
-  let views = {};
-
-  fetch('/clients')
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      // todo: only display current page if on dashboard
-      // json.clients.forEach(createClient);
-
-      // Handle socketio events
-      let socket = io('http://localhost:3000');
-      socket.on('news', function (data) {
-        console.log('news', data);
-        socket.emit('my other event', { my: 'data' });
-      });
-
-      socket.on('tinylr:destroy', function (data) {
-        let view = views[data.id];
-        if (!view) return;
-
-        console.log('destroy', data, view, views);
-        window.view = view;
-        view.remove();
-      });
-
-      socket.on('tinylr:create', function (data) {
-        console.log('create', data);
-        createClient(data);
-      });
+    // Handle socketio events
+    let socket = io('http://localhost:3000');
+    socket.on('news', function (data) {
+      console.log('news', data);
+      socket.emit('my other event', { my: 'data' });
     });
 
-    function createClient(client) {
-      let view = views[client.id] = new View({
-        name: 'client-' + client.id,
+    socket.on('tinylr:destroy', function (data) {
+      let view = views[data.id];
+      if (!view) return;
 
-        defaults: client
-      });
+      console.log('destroy', data, view, views);
+      view.remove();
+    });
 
-      console.log('create view', client);
-      view.appendTo(document.querySelector('.js-app'));
-    }
+    socket.on('tinylr:create', function (data) {
+      console.log('create', data);
+      createClient(data);
+    });
+  });
 
-}, 500);
+function createClient(client) {
+  let view = views[client.id] = new View({
+    name: 'client-' + client.id,
+    defaults: client
+  });
+
+  console.log('create view', client);
+  view.appendTo(document.querySelector('.js-app'));
+}
