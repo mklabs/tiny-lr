1d7e3043eb4b9c01245468be993c57fd3b618533
Mickael Daniel <daniel.mickael@gmail.com>
2016-08-11 20:51:47 +0200

    dashboard - remove debug socket.io news event

--- cli/assets/index.js
+++ cli/assets/index.js
@@ -14,11 +14,6 @@ fetch('/clients')
 
     // Handle socketio events
     let socket = io('http://localhost:3000');
-    socket.on('news', function (data) {
-      console.log('news', data);
-      socket.emit('my other event', { my: 'data' });
-    });
-
     socket.on('tinylr:destroy', function (data) {
       let view = views[data.id];
       if (!view) return;
